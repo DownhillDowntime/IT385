2957d5926bb4e899bebd1f4560194454ad386bb5
tgomez <tgomez4@mail.greenriver.edu>
2020-01-22 16:36:33 -0800

    get funct twice

--- fuctions.py
+++ fuctions.py
@@ -4,7 +4,13 @@
 def func1():
 	print("Getting Functy")
 
+#function that takes an argument
+def addNums(val1, val2):
+	print(val1 + val2)
+
 # call functions
-func1()
-print(func1())
-print(func1)
+#func1()
+#print(func1())
+#print(func1)
+
+addNums(7, 11)
