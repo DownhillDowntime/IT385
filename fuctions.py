97494834a4c505652c3a189178c1b3a25a2b6464
tgomez <tgomez4@mail.greenriver.edu>
2020-01-22 17:42:18 -0800

    get funct twice

--- fuctions.py
+++ fuctions.py
@@ -8,9 +8,40 @@ def func1():
 def addNums(val1, val2):
 	print(val1 + val2)
 
-# call functions
+#fuction with a default value
+def power(num, power=2):
+	result = 1
+	for i in range(power):
+		result = result * num
+	print(result)
+
+#function that returns a value
+def cube(x):
+	return x * x * x
+
+#function that adds variable number of  numbers
+def multi_add(*args):
+	result = 0
+	for x in args:
+		result = result + x
+	return result
+
+#call functions
+
+print(multi_add(6, 7))
+print(multi_add(15, 38, 22, 67))
+
+#cube(3)
+#print(cube(5))
+#myCube = cube(7)
+#print(myCube)
+
 #func1()
 #print(func1())
 #print(func1)
 
-addNums(7, 11)
+#addNums(7, 11)
+
+#power(5)
+#power(5, 2)
+#power(5, 3)
